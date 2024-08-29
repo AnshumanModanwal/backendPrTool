dde3a89e476b6a0d9673fa216de3f1b34ccd0063
AnshumanModanwal <anshumanmodanwal9628@gmail.com>
2024-08-29 19:18:55 +0530

    uploaded code of backend of payemnt portal and prtool

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ const port = process.env.PORT || 5000;
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 
-// // Configure CORS
-// app.use(cors({
-//   origin: 'https://www.bizzowl.com', // Allow requests from this domain
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
-//   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-//   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-// }));
-// app.options('/Orders', cors()); // Handle preflight
+// Configure CORS
+app.use(cors({
+  origin: 'https://www.bizzowl.com', // Allow requests from this domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  optionsSuccessStatus: 200, // For legacy browsers
+}));
+
+// Handle preflight requests for all routes
+app.options('*', cors()); 
+
 // Google AI API setup
 const MODEL_NAME = "models/text-bison-001";
 const API_KEY = process.env.GOOGLE_API_KEY;
