003ed6415d154d2d9f092527a4372d7ad84de26d
AnshumanModanwal <anshumanmodanwal9628@gmail.com>
2024-08-29 20:02:23 +0530

    uploaded code of backend of payemnt portal and prtool

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 // Configure CORS
-app.use(cors({
-  origin: '*', // Allow requests from this domain
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  optionsSuccessStatus: 200, // For legacy browsers
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // Handle preflight requests for all routes
 app.options('*', cors()); 
 
