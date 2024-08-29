49251105c547616c7a103382c47ecfd09e47a6b0
AnshumanModanwal <anshumanmodanwal9628@gmail.com>
2024-08-29 19:28:01 +0530

    uploaded code of backend of payemnt portal and prtool

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 // Configure CORS
-app.use(cors({
-  origin: 'https://www.bizzowl.com', // Allow requests from this domain
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  optionsSuccessStatus: 200, // For legacy browsers
-}));
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 
 // Handle preflight requests for all routes
 app.options('*', cors()); 
