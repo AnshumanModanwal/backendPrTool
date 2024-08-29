9eeddb42999d867be03cb628a26f39515577177a
AnshumanModanwal <anshumanmodanwal9628@gmail.com>
2024-08-29 20:13:13 +0530

    uploaded code of backend of payemnt portal and prtool

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const port = process.env.PORT || 5000;
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 
-// Configure CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-// Handle preflight requests for all routes
-app.options('*', cors()); 
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://www.bizzowl.com'], // List all allowed origins here
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  optionsSuccessStatus: 200, // For legacy browsers
+}));
+
+// Handle preflight requests
+app.options('*', cors());
 
 // Google AI API setup
 const MODEL_NAME = "models/text-bison-001";
